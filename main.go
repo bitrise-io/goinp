ad0c973226afeabc918d20bfecc7a041a0f19d27
Viktor Benei <viktor.benei@gmail.com>
2015-07-07 20:03:25 +0200

    made a proper package from it

--- main.go
+++ main.go
@@ -1,75 +1,26 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"io"
 	"log"
-	"os"
-	"strconv"
-	"strings"
+
+	"github.com/bitrise-io/go-interactive-cli/goinp"
 )
 
-func AskForStringFromReader(messageToPrint string, inputReader io.Reader) (string, error) {
-	scanner := bufio.NewScanner(inputReader)
-	fmt.Printf("%s : ", messageToPrint)
-	if scanner.Scan() {
-		scannedText := scanner.Text()
-		return scannedText, nil
-	}
-	return "", errors.New("Failed to get input - scanner failed.")
-}
-
-func AskForString(messageToPrint string) (string, error) {
-	return AskForStringFromReader(messageToPrint, os.Stdin)
-}
-
-func AskForIntFromReader(messageToPrint string, inputReader io.Reader) (int64, error) {
-	userInputStr, err := AskForStringFromReader(messageToPrint, inputReader)
-	if err != nil {
-		return 0, err
-	}
-	return strconv.ParseInt(userInputStr, 10, 64)
-}
-
-func AskForInt(messageToPrint string) (int64, error) {
-	return AskForIntFromReader(messageToPrint, os.Stdin)
-}
-
-func AskForBoolFromReader(messageToPrint string, inputReader io.Reader) (bool, error) {
-	userInputStr, err := AskForStringFromReader(messageToPrint, inputReader)
-	if err != nil {
-		return false, err
-	}
-	lowercased := strings.ToLower(userInputStr)
-	if lowercased == "yes" || lowercased == "y" {
-		return true, nil
-	}
-	if lowercased == "no" || lowercased == "n" {
-		return false, nil
-	}
-	return strconv.ParseBool(lowercased)
-}
-
-func AskForBool(messageToPrint string) (bool, error) {
-	return AskForBoolFromReader(messageToPrint, os.Stdin)
-}
-
 func main() {
-	retStr, err := AskForString("Please enter some text here")
+	retStr, err := goinp.AskForString("Please enter some text here")
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 	fmt.Println("Entered text was:", retStr)
 
-	retInt, err := AskForInt("Please enter a number")
+	retInt, err := goinp.AskForInt("Please enter a number")
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 	fmt.Println("Entered:", retInt)
 
-	retBool, err := AskForBool("Yes or no?")
+	retBool, err := goinp.AskForBool("Yes or no?")
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
