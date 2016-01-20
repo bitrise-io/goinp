64616810fb5437d716016b96cb6830a0fe520940
Viktor Benei <viktor.benei@gmail.com>
2016-01-20 22:29:51 +0100

    new method : SelectFromStrings

--- goinp/goinp_test.go
+++ goinp/goinp_test.go
@@ -3,6 +3,8 @@ package goinp
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestAskForStringFromReader(t *testing.T) {
@@ -11,9 +13,7 @@ func TestAskForStringFromReader(t *testing.T) {
 	testUserInput := "this is some text"
 
 	res, err := AskForStringFromReader("Enter some text", strings.NewReader(testUserInput))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if res != testUserInput {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", res, testUserInput)
 	}
@@ -25,9 +25,7 @@ func TestAskForIntFromReader(t *testing.T) {
 	testUserInput := "31"
 
 	res, err := AskForIntFromReader("Enter a number", strings.NewReader(testUserInput))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if res != 31 {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", res, testUserInput)
 	}
@@ -39,9 +37,7 @@ func TestAskForBoolFromReader(t *testing.T) {
 	// yes
 	testUserInput := "y"
 	res, err := AskForBoolFromReader("Yes or no?", strings.NewReader(testUserInput))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if res != true {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", res, testUserInput)
 	}
@@ -49,9 +45,7 @@ func TestAskForBoolFromReader(t *testing.T) {
 	// no
 	testUserInput = "no"
 	res, err = AskForBoolFromReader("Yes or no?", strings.NewReader(testUserInput))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if res != false {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", res, testUserInput)
 	}
@@ -61,9 +55,7 @@ func TestParseBool(t *testing.T) {
 	t.Log("Simple Yes")
 	testUserInput := "y"
 	isYes, err := ParseBool("YeS")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if !isYes {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", isYes, testUserInput)
 	}
@@ -71,9 +63,7 @@ func TestParseBool(t *testing.T) {
 	t.Log("Simple No")
 	testUserInput = "no"
 	isYes, err = ParseBool("n")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if isYes {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", isYes, testUserInput)
 	}
@@ -83,10 +73,46 @@ func TestParseBool(t *testing.T) {
  yes
 `
 	isYes, err = ParseBool(testUserInput)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if !isYes {
 		t.Fatalf("Scanned input (%s) does not match expected (%s)", isYes, testUserInput)
+	}
+}
+
+func TestSelectFromStringsFromReader(t *testing.T) {
+	availableOptions := []string{"first", "second", "third"}
+
+	//
+	{
+		_, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("-1"))
+		require.EqualError(t, err, "Invalid option: You entered a number less than 1")
+	}
+	//
+	{
+		_, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("0"))
+		require.EqualError(t, err, "Invalid option: You entered a number less than 1")
+	}
+	//
+	{
+		res, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("1"))
+		require.NoError(t, err)
+		require.Equal(t, "first", res)
+	}
+	//
+	{
+		res, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("2"))
+		require.NoError(t, err)
+		require.Equal(t, "second", res)
+	}
+	//
+	{
+		res, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("3"))
+		require.NoError(t, err)
+		require.Equal(t, "third", res)
+	}
+	//
+	{
+		_, err := SelectFromStringsFromReader("Select something", availableOptions, strings.NewReader("4"))
+		require.EqualError(t, err, "Invalid option: You entered a number greater than the last option's number")
 	}
 }
